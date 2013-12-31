c91d2a93a3290c7af5eab4dd67d97038dc848439
yarko <yarkot1@gmail.com>
2013-12-30 20:55:33 -0600

    wip - this is unsatisfying, too much impedence to get data out to request properly from mongo; going to shift to sqlalchemy for a while, see how that goes...

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from flask import Flask, render_template, request, jsonify, _app_ctx_stack
 from flask import redirect, url_for
 from flask.ext.mongokit import MongoKit, Document
 
-from flask_debugtoolbar import DebugToolbarExtension
+# from flask_debugtoolbar import DebugToolbarExtension
 
 #from sqlite3 import dbapi2 as sqlite3
 
@@ -16,7 +16,7 @@ SECRET_KEY = 'some super secret development key'
 app = Flask(__name__)
 app.config.from_object(__name__)
 
-toolbar = DebugToolbarExtension(app)
+# toolbar = DebugToolbarExtension(app)
 
 # the data model
 class Task(Document):
@@ -37,6 +37,35 @@ db.register([Task])
 # def get_db():
 #     return MongoKit(app)
 
+def mongoify(*args, **kwargs):
+    """Creates a :class:`~flask.Response` with the JSON representation of
+    the given arguments with an `application/json` mimetype.  The arguments
+    to this function are the same as to the :class:`dict` constructor.
+
+    Example usage::
+
+        @app.route('/_get_current_user')
+        def get_current_user():
+            return jsonify(username=g.user.username,
+                           email=g.user.email,
+                           id=g.user.id)
+
+    This will send a JSON response like this to the browser::
+
+        {
+            "username": "admin",
+            "email": "admin@localhost",
+            "id": 42
+        }
+
+    This requires Python 2.6 or an installed version of simplejson.  For
+    security reasons only objects are supported toplevel.  For more
+    information about this, have a look at :ref:`json-security`.
+
+    .. versionadded:: 0.2
+    """
+    return app.response_class(dict(*args, **kwargs), mimetype='application/json')
+
 #@app.teardown_appcontext
 # def close_database(exception):
 #   top = _app_ctx_stack.top
@@ -50,14 +79,25 @@ def index():
 @app.route('/tasks')
 def todos():
     entries = db.Task.find()
-    return jsonify(tasks=entries)
+    tasks = list(entries)
+    for i in tasks:
+        i.pop(u'_id')
+    # return jsonify(tasks=list(entries))
+    # return jsonify(tasks=[i.to_json() for i in tasks])
+    return mongoify(tasks=[i.to_json() for i in tasks])
 
 @app.route('/tasks/new', methods=['GET', 'POST'])
 def new_todo():
     if request.method == 'POST':
-        task = db.Task()
-        task.title = request.json['title']
-        task.description = request.json['description']
+        task = db.Task()   # create, w/ default creation;
+        ## one of two ways:
+        rtask = request.json   # creation is not part of it
+        # rtask.pop('creation')
+        ## alternately, could have done this:
+        # rtask = {}
+        # rtask.title = request.json['title']
+        # rtask.description = request.json['description']
+        task.update(rtask)     # update desired fields
         task.save()
         return redirect(url_for('index'))
     return jsonify({"title": request.json['title'],
